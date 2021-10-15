0fc806e0d285d90a396e3a823fa80888cdb3f1cf
Vinayak kamati <vinayakkamati123@gmail.com>
2021-10-15 20:59:07 +0530

    working on postman

--- src/main/java/com/vinayak/blog/controllers/LogInRegistrationController.java
+++ src/main/java/com/vinayak/blog/controllers/LogInRegistrationController.java
@@ -3,7 +3,6 @@ package com.vinayak.blog.controllers;
 import com.vinayak.blog.dto.UserDto;
 import com.vinayak.blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
